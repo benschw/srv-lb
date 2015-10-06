fee35296a467a49894c8a15c535525885b8acf2d
Ben Schwartz <benschw@gmail.com>
2015-10-06 13:25:53 -0500

    making round robin thread safe

---
+++ roundrobinclb/result_cache.go
@@ -0,0 +1,86 @@
+package roundrobinclb
+
+import (
+	"fmt"
+	"net"
+	"sort"
+	"sync"
+
+	"github.com/benschw/srv-lb/dns"
+)
+
+type AddressResult struct {
+	srvs []net.SRV
+	i    int
+}
+
+type ResultCache struct {
+	m map[string]*AddressResult
+	sync.RWMutex
+}
+
+func NewCache() *ResultCache {
+	return &ResultCache{
+		m: make(map[string]*AddressResult),
+	}
+}
+
+func (s *ResultCache) Next(key string, srvs []net.SRV) (net.SRV, error) {
+	s.Lock()
+	defer s.Unlock()
+
+	s.applyResults(key, srvs)
+
+	return s.getNextResult(key)
+}
+
+func (s *ResultCache) applyResults(key string, srvs []net.SRV) {
+	old, ok := s.m[key]
+	if !ok {
+		old = &AddressResult{
+			srvs: make([]net.SRV, 0),
+			i:    0,
+		}
+		s.m[key] = old
+	}
+	ordered, changed := s.mergeResults(old.srvs, srvs)
+	if changed {
+		s.m[key].srvs = ordered
+		s.m[key].i = 0
+	}
+}
+
+func (s *ResultCache) mergeResults(oldResults []net.SRV, newResults []net.SRV) ([]net.SRV, bool) {
+	sort.Sort(dns.ByTarget(newResults))
+
+	if len(oldResults) != len(newResults) {
+		return newResults, true
+	}
+
+	for i, val := range oldResults {
+		if newResults[i] != val {
+			return newResults, true
+		}
+	}
+
+	return newResults, false
+}
+
+func (s *ResultCache) getNextResult(key string) (net.SRV, error) {
+	var srv net.SRV
+	srvs := s.m[key].srvs
+	i := s.m[key].i
+
+	if len(srvs) == 0 {
+		return srv, fmt.Errorf("no results found for '%s'", key)
+	}
+
+	if len(srvs) <= i {
+		i = 0
+	}
+
+	srv = srvs[i]
+	s.m[key].i += 1
+
+	return srv, nil
+}
--- roundrobinclb/roundrobin.go
+++ roundrobinclb/roundrobin.go
@@ -1,43 +1,31 @@
 package roundrobinclb
 
-import (
-	"fmt"
-	"sort"
-
-	"github.com/benschw/srv-lb/dns"
-)
+import "github.com/benschw/srv-lb/dns"
 
 func New(lib dns.Lookup) *RoundRobinClb {
-	lb := new(RoundRobinClb)
-	lb.dnsLib = lib
-	lb.i = 0
-
-	return lb
+	return &RoundRobinClb{
+		dnsLib: lib,
+		cache:  NewCache(),
+	}
 }
 
 type RoundRobinClb struct {
 	dnsLib dns.Lookup
-	i      int
+	cache  *ResultCache
 }
 
 func (lb *RoundRobinClb) Next(name string) (dns.Address, error) {
-	add := dns.Address{}
+	var add dns.Address
 
 	srvs, err := lb.dnsLib.LookupSRV(name)
 	if err != nil {
 		return add, err
 	}
-	sort.Sort(dns.ByTarget(srvs))
 
-	if len(srvs) == 0 {
-		return add, fmt.Errorf("no SRV records found")
+	srv, err := lb.cache.Next(name, srvs)
+	if err != nil {
+		return add, err
 	}
-	if len(srvs)-1 < lb.i {
-		lb.i = 0
-	}
-	//	log.Printf("%d/%d / %+v", lb.i, len(srvs), srvs)
-	srv := srvs[lb.i]
-	lb.i = lb.i + 1
 
 	ip, err := lb.dnsLib.LookupA(srv.Target)
 	if err != nil {
